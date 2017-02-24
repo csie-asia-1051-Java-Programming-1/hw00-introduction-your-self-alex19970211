f2b5431cbfdd4cab9f3e7690a36b47c8a3819bae
alex19970211 <105021022@live.asia.edu.tw>
2017-02-24 15:02:06 +0800

    hw submit01

--- src/hw00.java
+++ src/hw00.java
@@ -1,11 +1,15 @@
 /*
+* Topic: 個人資料
 * Date: 2017/02/24
+* Author: 105021022 林暐峻
 */
 public class hw00 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+System.out.println("資工系(資電組)");
+System.out.println("林暐峻");
 System.out.print("105021022");
 
 	}
